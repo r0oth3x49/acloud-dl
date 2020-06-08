a936e03ad4192a25dc3419f8c73c332a547965c9
r0oth3x49 <r0oth3x49@gmail.com>
2020-06-08 16:24:41 +0500

    updated shebang to python3..

--- acloud/__init__.py
+++ acloud/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 __version__ = "0.1"
--- acloud/_sanitize.py
+++ acloud/_sanitize.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8
 
 from __future__ import unicode_literals
